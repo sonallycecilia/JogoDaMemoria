0457037be2c363aece7e09eed7bf946c8639d479
sonallycecilia <sonallycecilia11@gmail.com>
2025-04-24 05:00:16 -0300

    algo roda

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local carta = require("models.carta")
+
 -- Codigo para debugar, favor, não excluir!! 
 if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
     local lldebugger = require "lldebugger"
@@ -9,3 +11,20 @@ if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
     end
 end
 
+
+local minhaCarta = carta.novo(1, "images/cartas/lua.png", "images/verso.png")
+
+function love.load()
+    minhaCarta.x = 100
+    minhaCarta.y = 100
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if minhaCarta:clicada(x, y) then
+        minhaCarta:alternar()
+    end
+end
+
+function love.draw()
+    minhaCarta:draw()
+end
--- models/carta.lua
+++ models/carta.lua
@@ -1,24 +1,73 @@
 local carta = {}
 
 -- CONSTRUTOR
-function carta.novo(_id, _imagem)
-    return{
-        id = _id,
-        imagem = _imagem,
-        revelada = false
+function carta.novo(id, caminhoImagemFrente, caminhoImagemVerso)
+    -- Tenta carregar as imagens
+    local imagemFrente = love.graphics.newImage(caminhoImagemFrente)
+    local imagemVerso = love.graphics.newImage(caminhoImagemVerso)
+    
+    -- Verifica se as imagens foram carregadas corretamente
+    if not imagemFrente or not imagemVerso then
+        print("Erro ao carregar as imagens.")
+    end
+    
+    -- Cria a nova carta com dimensões fixas
+    local nova = {
+        id = id,
+        imagemFrente = imagemFrente,
+        imagemVerso = imagemVerso,
+        revelada = false,
+        largura = 250,  -- Defina o tamanho fixo desejado
+        altura = 310,   -- Defina o tamanho fixo desejado
+        x = 0,  -- Posição X da carta
+        y = 0   -- Posição Y da carta
     }
+    
+    -- Associa a tabela 'carta' à nova instância
+    setmetatable(nova, { __index = carta })
+    return nova
 end
 
 -- METODOS
-function carta.revelar()
-    return {
-        revelada = false
-    }
+function carta:revelar()
+    self.revelada = true
 end
 
-function carta.esconder()
-    return{
-        revelada = false
-    }
+function carta:esconder()
+    self.revelada = false
 end
 
+-- Método para desenhar a carta com redimensionamento
+function carta:draw()
+    if self.revelada then
+        if self.imagemFrente then
+            love.graphics.draw(self.imagemFrente, self.x, self.y, 0, self.largura / self.imagemFrente:getWidth(), self.altura / self.imagemFrente:getHeight())
+        else
+            print("Erro: A imagem da frente não foi carregada corretamente!")
+        end
+    else
+        if self.imagemVerso then
+            love.graphics.draw(self.imagemVerso, self.x, self.y, 0, self.largura / self.imagemVerso:getWidth(), self.altura / self.imagemVerso:getHeight())
+        else
+            love.graphics.setColor(0.2, 0.6, 0.8)  -- Cor do verso da carta
+            love.graphics.rectangle("fill", self.x, self.y, self.largura, self.altura)  -- Desenha um retângulo como verso
+            love.graphics.setColor(1, 1, 1)  -- Reseta a cor
+        end
+    end
+end
+
+-- Método para alternar entre frente e verso ao clicar
+function carta:alternar()
+    self.revelada = not self.revelada
+end
+
+-- Função para verificar se a carta foi clicada
+function carta:clicada(mx, my)
+    -- Verifica se o clique está dentro dos limites da carta
+    if mx >= self.x and mx <= self.x + self.largura and my >= self.y and my <= self.y + self.altura then
+        return true
+    end
+    return false
+end
+
+return carta
