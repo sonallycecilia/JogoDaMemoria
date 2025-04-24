4dfa0e1dd106caea6ddbe5d742dd170eadbd14e9
sonallycecilia <sonallycecilia11@gmail.com>
2025-04-24 00:43:07 -0300

    teste

---
+++ main.lua
@@ -0,0 +1,12 @@
+-- Codigo para debugar, favor, não excluir!! 
+if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
+    local lldebugger = require "lldebugger"
+    lldebugger.start()
+    local run = love.run
+    function love.run(...)
+        local f = lldebugger.call(run, false, ...)
+        return function(...) return lldebugger.call(f, false, ...) end
+    end
+end
+
+print("ola")
