f249625ca12fbcab6dc68668775d5369c2c15c31
Thalubinho <thaleshenriquest@gmail.com>
2025-06-02 11:19:30 -0300

    Alguns métodos de adversário

--- inteligencia_maquina/adversario.lua
+++ inteligencia_maquina/adversario.lua
@@ -1,4 +1,33 @@
 require("")
 
-local adversario = {memoria = {}, }
+local adversario = {memoria = {}}
 
+function adversario:inicializarMemoria(linhas, colunas)
+    local l = {}
+    for i = 1, linhas, 1 do
+        for j = 1, colunas, 1 do
+            l[j] = "X"
+        end
+        adversario.memoria[i] = l
+        l = {}
+    end
+end
+
+function adversario:exibirMemoria()
+    for i = 1, #adversario.memoria, 1 do
+        for j = 1, #adversario.memoria[i], 1 do
+            io.write(adversario[i][j], " ")
+        end
+        io.write("\n")
+    end
+end
+
+function adversario:selecionarCarta()
+end
+
+function adversario:buscarPar(memoriaJogador)
+end
+
+adversario:inicializarMemoria(  )
+adversario:inicializarMemoria()
+    
