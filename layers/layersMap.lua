91aec86738539b0748976d05de3b6b497a106ec8
sonallycecilia <sonallycecilia11@gmail.com>
2025-06-04 11:51:20 -0300

    partida criada

--- layers/layersMap.lua
+++ layers/layersMap.lua
@@ -1,6 +1,7 @@
 local layerMap = {
     menuPrincipal = require("layers.menuPrincipalLayer"),
     menuJogo = require("layers.menuJogoLayer"),
+    partida = require("layers.layerPartida"),
     -- outras layers aqui...
 }
 
