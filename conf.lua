ec163fc4a15187d30aed7b57727fc1e04ea6cac0
Thalubinho <thaleshenriquest@gmail.com>
2025-06-10 14:26:48 -0300

    Deixando o jogo em modo janela temporariamente

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.title = "Jogo da Memoria"
     t.window.resizable = false
-    t.window.fullscreen = true
+    t.window.fullscreen = false -- Alterei temporariamente
     t.window.vsync = true
 end
