335b798902fd33f04eb1c399abf8fbf77c592c59
Thalubinho <thaleshenriquest@gmail.com>
2025-06-10 13:46:41 -0300

    Criando classe jogador e adicionando métodos básicos

---
+++ classes/jogador.lua
@@ -0,0 +1,18 @@
+require("classes.utils.score")
+
+Jogador = {
+    nome = "Convidado",
+    paresEncontrados = 0,
+    score = Score:new(nivel)
+}
+Jogador.__index = Jogador
+
+function Jogador:new(nivel)
+    local novoJogador ={
+        score = Score:new(nivel)
+    }    
+
+    setmetatable(novoJogador, Jogador)
+
+    return novoJogador
+end 
