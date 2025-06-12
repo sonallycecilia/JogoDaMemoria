4a67f2121dd11cc535afe3367d337610225787c7
Thalubinho <thaleshenriquest@gmail.com>
2025-06-11 22:35:24 -0300

    Removendo o subprojeto

--- JogoDaMemoria-main/conf.lua
+++
@@ -1,6 +0,0 @@
-function love.conf(t)
-    t.window.title = "Jogo da Memória"
-    t.window.width = 1600
-    t.window.height = 900
-    t.window.resizable = false
-end
--- JogoDaMemoria-main/layers/layersMap.lua
+++
@@ -1,8 +0,0 @@
--- layers/layersMap.lua
-return {
-    menuPrincipal = require("layers.menuPrincipalLayer"),
-    menuJogo = require("layers.menuJogoLayer"),
-    selecaoNivel = require("layers.selecaoNivelLayer"),
-    partida = require("layers.partidaLayer"),
-    jogoCooperativo = require("layers.jogoCooperativo") -- ADICIONADO
-}
