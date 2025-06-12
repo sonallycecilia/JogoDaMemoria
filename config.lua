eede21e7d29f9a57de8b9aa22fef30a325412eee
sonallycecilia <sonallycecilia11@gmail.com>
2025-06-12 14:00:58 -0300

    mudando png de cartas

--- config.lua
+++ config.lua
@@ -51,16 +51,16 @@ Config.botoes = {
 Config.deck = {
     "midia/images/cartas/fada.png",
     "midia/images/cartas/naly.png",
-    "midia/images/cartas/elfa.png",
     "midia/images/cartas/draenei.png",
-    "midia/images/cartas/borboleta.png",
-    "midia/images/cartas/lua.png",
-    "midia/images/cartas/coracao.png",
-    "midia/images/cartas/espelho.png", -- Observação: você tem draenei duplicado
-    "midia/images/cartas/flor.png",
-    "midia/images/cartas/gato.png",
-    "midia/images/cartas/pocao.png",
-    "midia/images/cartas/planta.png",
+    "midia/images/cartas/thales.png",
+    "midia/images/cartas/lucy.png",
+    "midia/images/cartas/fury.png",
+    "midia/images/cartas/fada.png",
+    "midia/images/cartas/naly.png",
+    "midia/images/cartas/draenei.png",
+    "midia/images/cartas/thales.png",
+    "midia/images/cartas/lucy.png",
+    "midia/images/cartas/fury.png",
 }
 
 -- Frames (MANTIDOS)
